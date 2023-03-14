5ae8a035673f30924d6177aa24542cd0b7360a59
Cristhian Vega C <61928138+cvegac@users.noreply.github.com>
2023-03-14 01:38:49 -0500

    Create exercise.js

---
+++ exercise.js
@@ -0,0 +1,29 @@
+let n = 2;
+
+if (n < 1 || n > 4) {
+    console.log("ERROR");
+}
+else {
+    let lengthSideMax = n * 4 - 1;
+    let array = new Array(lengthSideMax);
+    for (let i = 0; i < lengthSideMax; i++) {
+        array[i] = new Array(lengthSideMax).fill(" ");
+    }
+    for (let i = 1; i <= n; i++) {
+        let initPoint = (n - i) * 2;
+        let finishPoint = lengthSideMax - initPoint;
+        for (let f = initPoint; f < finishPoint; f++) {
+            for (let c = initPoint; c < finishPoint; c++) {
+                if (f == initPoint || c == initPoint) {
+                    array[f][c] = "*";
+                }
+                if (f == finishPoint - 1 || c == finishPoint - 1) {
+                    array[f][c] = "*";
+                }
+            }
+        }
+    }
+    for (let i = 0; i < lengthSideMax; i++) {
+        console.log(array[i].join(""));
+    }
+}
