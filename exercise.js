d8a9339fe01a47af6e36edb8b63f27b53875f4e6
Cristhian Vega C <61928138+cvegac@users.noreply.github.com>
2023-03-14 01:39:20 -0500

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,4 +1,4 @@
-let n = 2;
+let n = 3;
 
 if (n < 1 || n > 4) {
     console.log("ERROR");
@@ -9,8 +9,8 @@ else {
     for (let i = 0; i < lengthSideMax; i++) {
         array[i] = new Array(lengthSideMax).fill(" ");
     }
-    for (let i = 1; i <= n; i++) {
-        let initPoint = (n - i) * 2;
+    for (let i = 0; i < n; i++) {
+        let initPoint = i * 2;
         let finishPoint = lengthSideMax - initPoint;
         for (let f = initPoint; f < finishPoint; f++) {
             for (let c = initPoint; c < finishPoint; c++) {
